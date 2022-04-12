e519bbd0341fa4fa1254dda1426e19bbccde4059
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-04-12 08:31:37 -0300

    Trabalhando com Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';//Prepara a Aplicação para ser executada em Browser
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
-  declarations: [
+  declarations: [//Dentro do declarations listamos todos os componentes, diretivas e pipes que vamos utilizar nesse módulo
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
-  imports: [
+  imports: [// Colocamos outros módulos que queremos utilizar nesse módulo.(ngModule)
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],// Colocamos os serviços que ficarão disponíveis para todos os componentes
+  bootstrap: [AppComponent] // É o componente que é instanciado ao iniciar a aplicação;
 })
 export class AppModule { }
